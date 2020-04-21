cf8d575b77020cae252c0284d3538efa57f69734
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-04-21 13:33:10 +0900

    3.0. state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Fav({ name, picture }) {
-  return (
-    <div>
-      <h2>What i like</h2>
-      <h3>{name}</h3>
-      <img src={picture} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
 
-const whatILike = [
-  {
-    name: "code",
-    image:
-      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-  },
-  {
-    name: "code1",
-    image:
-      "https://images.unsplash.com/photo-1517148815978-75f6acaaf32c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-  },
-  {
-    name: "coder",
-    image:
-      "https://images.unsplash.com/photo-1544256718-3bcf237f3974?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQwMzA0fQ&auto=format&fit=crop&w=1051&q=80"
+  minus = () => {
+    console.log("minus");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hi! {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function renderWhatILike(eachItem) {
-  return <Fav name={eachItem.name} picture={eachItem.image} />;
-}
-
-function App() {
-  return <div>{whatILike.map(renderWhatILike)}</div>;
 }
 
 export default App;
