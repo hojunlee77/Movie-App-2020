b76f69a5fcf37bfd9a6c13796ab9ac3ee09a5c7b
junho <hojunlee77@gmail.com>
2020-06-09 20:32:00 +0900

    reactJS free course finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./components/Detail";
+import "./App.css";
 
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
